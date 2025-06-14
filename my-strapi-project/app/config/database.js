c9c4be67bdc8067ffe047251f8836e97db04dcb7
Snigdha Singh <snigdha0903singh@gmail.com>
2025-06-14 16:28:25 +0530

    structure-change

--- my-strapi-project/app/config/database.js
+++ my-strapi-project/app/config/database.js
@@ -25,11 +25,11 @@ module.exports = ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'Postgres'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
+        database: env('DATABASE_NAME', 'strapi_db'),
         user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'password1234'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
